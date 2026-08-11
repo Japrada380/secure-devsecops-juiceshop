d3bfdc1432485d070f38574b67e87f01e39fcdee
Jhon Alex Prada Gomez <pradajhonalex@gmail.com>
2026-08-10 22:42:47 -0500

    fix(terraform): define KMS key policy

--- terraform/modules/security-baseline/kms.tf
+++ terraform/modules/security-baseline/kms.tf
@@ -1,7 +1,52 @@
+##############################################
+# KMS KEY FOR S3 / CLOUDTRAIL
+##############################################
+
+data "aws_caller_identity" "current" {}
+
 resource "aws_kms_key" "s3" {
   description             = "CMK for S3 encryption"
   enable_key_rotation     = true
   deletion_window_in_days = 30
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+
+      {
+        Sid = "EnableRootPermissions"
+
+        Effect = "Allow"
+
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+
+        Action = "kms:*"
+
+        Resource = "*"
+      },
+
+      {
+        Sid = "AllowCloudTrail"
+
+        Effect = "Allow"
+
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+
+        Action = [
+          "kms:GenerateDataKey*",
+          "kms:Decrypt",
+          "kms:DescribeKey"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
 
   tags = merge(
     var.tags,
